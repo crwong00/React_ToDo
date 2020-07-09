582e3901b65058c3fa1f2d3e85f7e3fb92e9ac6a
crwong00 <crwong00@outlook.com>
2020-07-09 13:18:33 -0500

    Reformat the React Template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component{
+  //Above is a class called App that extends the functionality of teh component class.
 
-export default App;
+  // export keyword makes the class available for use outside this file. (like an access modifier in c#)
+
+
+constructor(){
+  //WE are going to create state data for our component. To do that, we need to create a constructor method.
+  //this method  will get called when an object is created using this class. Said another way,
+  //This method will be called when the component is initialized. 
+
+  //To ensure that we have all the necessary  features from React to create a stateful component, we need to cal a method called super()
+  //This super() calls teh constructor for the component class in react.
+  super();
+
+}
+  // using '=>' (lambda) syntax the return is implied and the return keyword is not needed. The scope around the body of the function is also not needed.
+  render = () => 
+  <div>
+
+  </div>
+};
